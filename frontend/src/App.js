80dbe786387682d9c218bf3a61490df010a69b37
Plinko183 <jrplinko@gmail.com>
2025-09-24 09:21:47 +0100

    addedmetrics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"; // 👈 aqui
 
 function App() {
   return (
@@ -11,6 +13,12 @@ function App() {
           <Route path="/" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
+
+      {/* 👇 performance */}
+      <SpeedInsights />
+
+      {/* 👇 tráfego */}
+      <Analytics />
     </div>
   );
 }
